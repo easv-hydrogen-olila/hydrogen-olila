8e56e74eda5c1ef9fc498ce32dd60554a669e7f7
Hamed-gharibi <vape5721@gmail.com>
2022-10-31 11:15:40 +0100

    Shopify token
    Shopify token

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -2,8 +2,8 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'https://info-6737.myshopify.com/',
+    storefrontToken: 'dbb5b22572c02a644d67f5591b618977',
     storefrontApiVersion: '2022-07',
   },
 });
