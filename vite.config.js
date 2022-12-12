17b032c62298ef3dc2a00e46b0328cb92cbe85c7
andommar <andommartinez@gmail.com>
2022-12-12 20:55:11 +0100

    Product page. SwiperJS product images slideshow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import hydrogen from '@shopify/hydrogen/plugin';
 import netlifyPlugin from '@netlify/hydrogen-platform/plugin'
 
 export default defineConfig({
-  plugins: [hydrogen(), netlifyPlugin()],
+  plugins: [hydrogen(), netlifyPlugin(),     {
+    name: 'remove-swiper',
+    transform(code, id, options = {}) {
+      if (options.ssr)
+        return code.replace(/import .swiper\/(s?css|less).*$/gm, '')
+    },
+  },],
+  
 });
