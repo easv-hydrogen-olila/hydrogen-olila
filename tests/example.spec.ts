e75f40fa1dc37d0e5955910f718a98260be64d12
andommar <andommartinez@gmail.com>
2023-01-01 02:32:33 +0100

    Unit test update fix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://aesthetic-cocada-7a12ce.netlify.app');
 
-  // Expect a title "to contain" a substring.
+  
   await expect(page).toHaveTitle(/Olila - Olila/);
 });
 
